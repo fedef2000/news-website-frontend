bc909d07911775974acf55f5ca075f9572f4bc15
fedef2000 <fedef306@gmail.com>
2022-08-25 17:35:46 +0200

    changed single article css on mobile

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return(
         <nav className="navbar">
             <a href="/">
-                <img alt="" src="https://www.comune.roisan.ao.it/wp-content/uploads/2021/05/sindaco.png"></img>
+                <img alt="" src="https://i.ibb.co/qYBkDtK/logo.png"></img>
             </a>
             <a href="/">
                 <h1>Sindaco del calciomercato</h1>
